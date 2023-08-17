2403c939cbdcd9567147de60ba19022b51dc92c3
user <samuelsouza.t@hotmail.com>
2023-08-17 18:43:32 -0300

    First commit, project created

---
+++ src/main/java/com/samueldev/workshopmongo/WorkshopSpringboot3MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.samueldev.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringboot3MongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringboot3MongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samueldev/workshopmongo/WorkshopSpringboot3MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samueldev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3MongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
